1d7316d2b2ade6d6791592e9da91f69c779ac337
Julien Ergan <julien.ergan.code@gmail.com>
2020-09-23 19:51:10 +0900

    proptype - progress 1

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// components
+import Hello from './components/hello';
+
 import '../assets/stylesheets/application.scss';
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
+// const Hello = ({ name }) => {
+//   return (
+//     <div>
+//       Hello,
+//       {name}
+//     </div>
+//   );
+// };
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+  ReactDOM.render(<Hello name={["Saki", "Julien"]}/>, root);
 }
